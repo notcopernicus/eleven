6985121b84f2b01aa69e8a84e0b5f5ee699ebb6e
notcopernicus <mhcopernic@gmail.com>
2025-11-01 00:43:57 -0400

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add event listeners to handle fade-out effect during navigation
+window.addEventListener('beforeunload', () => {
+    document.body.classList.add('fade-out');
+});
+
+document.body.addEventListener('click', () => {
+    if (window.location.pathname === '/page1.html') {
+        window.location.href = 'page2.html';
+    } else if (window.location.pathname === '/page2.html') {
+        window.location.href = 'page3.html';
+    } else if (window.location.pathname === '/page3.html') {
+        window.location.href = 'page4.html';
+    } else if (window.location.pathname =='/page4.html')
+        window.location.href = 'page1.html';
+});
+
