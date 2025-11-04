396c3738d146ed744365fa7a44860a071af5f424
notcopernicus <mhcopernic@gmail.com>
2025-11-04 00:35:07 -0500

    edits

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ window.addEventListener('beforeunload', () => {
 });
 
 document.body.addEventListener('click', () => {
-    if (window.location.pathname === '/page1.html') {
+    if (window.location.pathname === '/index.html') {
         window.location.href = 'page2.html';
     } else if (window.location.pathname === '/page2.html') {
         window.location.href = 'page3.html';
     } else if (window.location.pathname === '/page3.html') {
         window.location.href = 'page4.html';
     } else if (window.location.pathname =='/page4.html')
-        window.location.href = 'page1.html';
+        window.location.href = 'index.html';
 });
 
