a42333ed45eded22f53c5b87e8fe751355307da6
copernicmh <mhcopernic@gmail.com>
2026-06-14 11:54:04 -0400

    finally

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ document.body.addEventListener('click', (e) => {
 
     // normalize to the filename (handle '/', '/index.html', and paths with folders)
     const lastSegment = window.location.pathname.split('/').pop();
-    const page = lastSegment === '' ? 'index.html' : lastSegment;
+    const page = lastSegment === '' ? '1.html' : lastSegment;
 
     const nextMap = {
-        'index.html': 'page2.html',
-        'page2.html': 'index.html',
+        '1.html': '2.html',
+        '2.html': '1.html',
     };
 
-    const next = nextMap[page] || 'index.html';
+    const next = nextMap[page] || '1.html';
     window.location.href = next;
 });
 
